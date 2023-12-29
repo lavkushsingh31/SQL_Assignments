221e408c173b920a0dcd3612bd3446553f462312
lavkushsingh31 <luvkushsynghh@gmail.com>
2023-12-29 06:21:45 +0530

    added more Practice queries - Day 4

--- Practice SQL Queries/Day_4_queries.sql
+++ Practice SQL Queries/Day_4_queries.sql
@@ -60,4 +60,47 @@ FROM customers
 GROUP BY Country, City
 ORDER BY TotalCustomers DESC;
 
+# Having is used to filter results after Group by
+SELECT CustomerID, SUM(TotalPaid) AS total_order_value
+FROM orders
+GROUP BY CustomerID
+HAVING total_order_value >= 1000
+ORDER BY total_order_value DESC;
 
+
+SELECT CustomerID, COUNT(OrderID) as Count_of_Orders
+FROM orders
+GROUP BY CUstomerID
+HAVING Count_of_Orders > 5
+ORDER BY Count_of_Orders DESC;
+
+SELECT CustomerID, AVG(TotalPaid) as average_spent
+FROM orders
+GROUP BY CustomerID;
+
+# subqueries
+# find the products sold more than its average
+SELECT ProductID, SUM(Quantity) as TotalQuantitySold
+FROM orderdetails
+GROUP BY ProductID
+HAVING TotalQuantitySold > (
+	SELECT AVG(Quantity) FROM orderdetails
+);
+
+# Monthly Sales per year
+SELECT Year(OrderDate) as Year, Month(OrderDate) as Month, SUM(TotalPaid) as TotalSales
+FROM orders
+GROUP BY Year, Month;
+
+#Average Sales per customer
+SELECT CustomerID, SUM(TotalPaid) as TotalPaid
+FROM orders
+GROUP BY CustomerID;
+
+# Highest and Lowest Selling Products (NOT WORKING)
+SELECT ProductID, SUM(Quantity) as QuantitySold
+FROM orderdetails
+GROUP BY ProductID
+HAVING QuantitySold = MIN(QuantitySold) ;
+#OR QuantitySold = MIN(QuantitySold) 
+#ORDER BY QuantitySold DESC;
